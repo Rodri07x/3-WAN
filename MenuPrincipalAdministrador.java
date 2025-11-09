755066561c8f46a7d37dfbe638d3da99e1a687a5
Rodrigo Martin <rodmartinzarzo@gmail.com>
2025-11-09 18:47:27 +0100

    Añadido variable y switch de opcionEventos

--- MenuPrincipalAdministrador.java
+++ MenuPrincipalAdministrador.java
@@ -60,3 +60,45 @@ public class MenuPrincipalAdministrador {
                             System.out.println("Opción mala, escriba un número del 1 al 4");
                     }
                     break;
+                case 2:
+                    System.out.println();
+                    System.out.println("--Sección Ciudades--"); //Opciones para ciudades de la agenda -- Rodrigo Martin
+                    System.out.println("Escriba una acción (1-4)");
+                    System.out.println("1.Registrar ciudad");
+                    System.out.println("2.Ciudades registradas");
+                    System.out.println("3.Actualizar ciudad");
+                    System.out.println("4.Eliminar ciudad");
+                    int opcionCiudades = sc.nextInt();
+                    sc.nextLine();
+                    switch (opcionCiudades) {
+                        case 1:
+                            System.out.println("Ingrese nombre de la ciudad");
+                            String nombreCiudad = sc.nextLine();
+                            System.out.println("Ingrese país de la ciudad");
+                            String paisCiudad = sc.nextLine();
+                            System.out.println("Ciudad registrada exitosamente");
+                            break;
+                        case 2:
+                            System.out.println("Listado de ciudades:");
+                            System.out.println("*");
+                            System.out.println("*");
+                            System.out.println("*");
+                            System.out.println("*");
+                            System.out.println("*");
+                            break;
+                        case 3:
+                            System.out.println("Ingrese el nombre de la ciudad a actualizar");
+                            String ciudadActualizar = sc.nextLine();
+                            System.out.println("Ingrese nuevos datos de la ciudad");
+                            String datosCiudad = sc.nextLine();
+                            System.out.println("Ciudad actualizada");
+                            break;
+                        case 4:
+                            System.out.println("Ingrese el nombre de la ciudad a eliminar");
+                            String ciudadEliminar = sc.nextLine();
+                            System.out.println("Ciudad eliminada");
+                            break;
+                        default:
+                            System.out.println("Opción mala, escriba un número del 1 al 4");
+                    }
+                    break;
