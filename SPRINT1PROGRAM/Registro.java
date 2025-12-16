6b9492a01f8dae5ab0d72f7524ac4751fbafcdfe
Rodrigo Martin <rodmartinzarzo@gmail.com>
2025-12-16 10:34:34 +0100

    Añadir carpeta

---
+++ SPRINT1PROGRAM/Registro.java
@@ -0,0 +1,45 @@
+package RETO2.SPRINT1PROGRAM.Sprint4;
+
+
+import java.util.Scanner;
+
+public class Registro {
+    public static void main(String[] args) {
+        String[] usuarios = {"Daniel", "Telmo", null};
+        String[] contraseñas = {"claveDaniel", "claveTelmo", null};
+
+        boolean registro = true;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Registro");
+        while (registro) {
+            System.out.println("Usuario:");
+            String usuario = sc.nextLine();
+
+            if (usuario.length() > 15) {  // Comprobar que lo que ponga el usuario sea menos de 15 caracteres -- Rodrigo
+                System.out.println("Error, el usuario debe de tener menos de 15 caracteres");
+                System.out.println("Inténtelo de nuevo");
+            }
+
+            System.out.println("Contraseña:");
+            String contraseña = sc.nextLine();
+            System.out.println("Confirmar contraseña:");
+            String confirmarcontraseña = sc.nextLine();
+
+            if (!contraseña.equals(confirmarcontraseña)) { // Que la constraseña sea igual a la puesta en confirmar contraseña -- Rodrigo
+                System.out.println("Error, las contraseñas no coinciden");
+                System.out.println("Inténtelo de nuevo");
+            }
+
+            usuarios[2] = usuario;
+            contraseñas[2] = contraseña;
+            System.out.println("Registro exitoso. Usuarios registrados:");
+            for (int i = 0; i < usuarios.length; i++) {
+                System.out.println("- " + usuarios[i]);
+            }
+            registro = false;
+        }
+
+        sc.close(); // Terminamos el Scanner aqui porque lo dijo Egoitz ya que podria dar bugs-- Rodrigo
+    }
+}
