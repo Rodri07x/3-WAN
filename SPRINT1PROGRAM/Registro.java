5640ef3659ea74eaacaf1e01e306bdd00002d57e
Rodrigo Martin <ikdca@plaiaundi.net>
2025-12-17 08:24:46 +0100

    Añadido arrayist al Registro

--- SPRINT1PROGRAM/Registro.java
+++ SPRINT1PROGRAM/Registro.java
@@ -1,12 +1,18 @@
 package RETO2.SPRINT1PROGRAM;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Registro {
     public static void main(String[] args) {
-        String[] usuarios = {"Daniel", "Telmo", null};
-        String[] contraseñas = {"claveDaniel", "claveTelmo", null};
+        ArrayList<String> usuarios = new ArrayList<>();
+        usuarios.add("Daniel");
+        usuarios.add("Telmo");
+
+        ArrayList<String> contraseñas = new ArrayList<>();
+        contraseñas.add("claveDaniel");
+        contraseñas.add("claveTelmo");
 
         boolean registro = true;
         Scanner sc = new Scanner(System.in);
@@ -19,6 +25,7 @@ public class Registro {
             if (usuario.length() > 15) {  // Comprobar que lo que ponga el usuario sea menos de 15 caracteres -- Rodrigo
                 System.out.println("Error, el usuario debe de tener menos de 15 caracteres");
                 System.out.println("Inténtelo de nuevo");
+                continue;
             }
 
             System.out.println("Contraseña:");
@@ -29,13 +36,14 @@ public class Registro {
             if (!contraseña.equals(confirmarcontraseña)) { // Que la constraseña sea igual a la puesta en confirmar contraseña -- Rodrigo
                 System.out.println("Error, las contraseñas no coinciden");
                 System.out.println("Inténtelo de nuevo");
+                continue;
             }
 
-            usuarios[2] = usuario;
-            contraseñas[2] = contraseña;
+            usuarios.add(usuario);
+            contraseñas.add(contraseña);
             System.out.println("Registro exitoso. Usuarios registrados:");
-            for (int i = 0; i < usuarios.length; i++) {
-                System.out.println("- " + usuarios[i]);
+            for (int i = 0; i < usuarios.size(); i++) {
+                System.out.println("- " + usuarios.get(i));
             }
             registro = false;
         }
